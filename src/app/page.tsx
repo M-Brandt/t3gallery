114f7c4cf76c37d44df3ffee90cd3912c31e6606
Michael Brandt <michaelbrandt5874@gmail.com>
2025-05-24 23:56:08 +0200

    Add DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://e540edk8dg.ufs.sh/f/VTXAOElZAeIOAkLJBf7wmnRh76ApOSkj3fqCbgTzZ28vY0GD",
@@ -12,12 +13,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <div>{post.name}</div>
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt={`Image ${image.id}`} />
           </div>
         ))}
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,7 +1,11 @@
-import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
 import { env } from "~/env";
+
+import { neon } from "@neondatabase/serverless";
+
+import { drizzle } from "drizzle-orm/neon-http";
+
 import * as schema from "./schema";
 
 /**
@@ -12,7 +16,8 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
+const sql = neon(process.env.POSTGRES_URL!);
 
-export const db = drizzle(conn, { schema });
+export const db = drizzle(sql, { schema });
